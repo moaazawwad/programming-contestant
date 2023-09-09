4864b912ed832f317f13a714c82ca4ef98d621f9
Amgad Hussein <amgadhussain.iqranetwork@gmail.com>
2023-09-09 14:22:25 +0300

    end of string \0

--- competitor/novice/cpp/variables-and-literals/strings.cpp
+++ competitor/novice/cpp/variables-and-literals/strings.cpp
@@ -8,15 +8,15 @@ int main(int argc, char const *argv[]) {
    * String is an array of characters.
    * The last element in the array is equal to '\0', which is a null character.
    */
+
   char name[100] = "Hello, World";
   // ['H', 'a', 'l', 'l', 'o', ',', ' ', 'W', 'o', 'r', 'l', 'd', '\0', '']
   cout << name << '\n';
 
-  name[12] = '\0';
+  name[8] = 'd';
   cout << name << '\n';
 
-  name[8] = 'd';
-  name[10] = '\0';
+  name[9] = '\0';
   // ['H', 'a', 'l', 'l', 'o', ',', ' ', 'W', 'd', 'r', '\0', 'd', '\0', '']
   cout << name << '\n';
 
